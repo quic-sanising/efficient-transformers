d07d3c66c96e03bf08660b66c107a050cea85d1e
quic-sanising <sanising@qti.qualcomm.com>
2025-02-05 14:05:52 -0800

    Replace tiny with min

--- QEfficient/transformers/sampler/test_sampler/vllm_sampler_topkp.py
+++ QEfficient/transformers/sampler/test_sampler/vllm_sampler_topkp.py
@@ -96,7 +96,7 @@ def apply_top_k_top_p(
         # Get all the top_k values.
         top_k_mask = logits_sort.gather(1, top_k_mask.unsqueeze(dim=1))
         top_k_mask = logits_sort < top_k_mask
-        logits_sort.masked_fill_(top_k_mask, torch.finfo(torch.float16).tiny)
+        logits_sort.masked_fill_(top_k_mask, torch.finfo(torch.float16).min)
 
     if not no_top_p:
         # Apply top-p.
@@ -105,7 +105,7 @@ def apply_top_k_top_p(
         top_p_mask = probs_sum <= 1 - p.unsqueeze(dim=1)
         # at least one
         top_p_mask[:, -1] = False
-        logits_sort.masked_fill_(top_p_mask, torch.finfo(torch.float16).tiny)
+        logits_sort.masked_fill_(top_p_mask, torch.finfo(torch.float16).min)
 
     # Re-sort the probabilities.
     logits = logits_sort.scatter(dim=-1, index=logits_idx, src=logits_sort)
